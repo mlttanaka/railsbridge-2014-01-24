198184379c26e289e2abe377d30d024941e571f8
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2014-01-25 10:17:50 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Monkeydo::Application.config.secret_key_base = '3dfd4a52a435c30a35518c2259e77583ed2c38fd3ee47158716da9f547b43762e55ac3d50f97d1898336a084aee40bb5f95986d6c772137ac0769979d68f7475'
