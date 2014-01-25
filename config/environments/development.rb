2fe78dec5949b138ff7d29073e766073884ea3b9
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2014-01-25 10:34:30 -0800

    Install Devise.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Monkeydo::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+  # Devise stuff
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
